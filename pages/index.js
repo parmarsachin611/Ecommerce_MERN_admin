121ae60fd234914606260e422abecf397f41edea
sachinparmar611 <parmarsachin707@gmail.com>
2023-05-05 17:48:33 +0530

    Admin Auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import { FcGoogle } from "react-icons/fc";
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  const { data: session } = useSession()
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
+          <button onClick={() => signIn('google')} className="flex items-center justify-center bg-white px-7 py-3 uppercase text-sm font-medium  shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+            <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
+            Login With Google
+          </button>
+      </div>
+    )
+  } else {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
 }
